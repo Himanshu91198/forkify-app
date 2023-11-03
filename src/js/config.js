312fcb8365016a5174b5e2c72b70973f303509f3
Himanshu Auti <auti.himanshu9@gmail.com>
2023-11-03 23:43:09 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a75b84f5-c09b-4e69-bbed-86ad7d0ee65b';
+export const MODAL_CLOSE_SEC = 2.5;
